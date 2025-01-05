f0d537c114ebebc1a93207ea3e5d338d2545b451
faqih <faqih.syakir11@gmail.com>
2025-01-05 19:10:49 +0700

    Add : models and Migrations

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('occupation');
+            $table->string('photo');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
