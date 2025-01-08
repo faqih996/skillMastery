f595db9ba36040d537e010e41b71a1a7fa76faeb
faqih <faqih.syakir11@gmail.com>
2025-01-08 18:24:35 +0700

    fecth : Model and ORM

--- app/Models/CourseBenefit.php
+++ app/Models/CourseBenefit.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CourseBenefit extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'course_id',
+    ];
+
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
 }
--- database/migrations/2025_01_05_024533_create_section_contents_table.php
+++ database/migrations/2025_01_05_024533_create_section_contents_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('course_contents', function (Blueprint $table) {
+        Schema::create('section_contents', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->text('content');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('course_contents');
+        Schema::dropIfExists('section_contents');
     }
 };
